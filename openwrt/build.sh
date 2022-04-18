910f0a0c44f0e5218d581482f0cad31d2fe646be
Grell Gragham <vrt.epm@protonmail.com>
2022-04-18 15:23:03 +0300

    Checkout new openwrt version

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git clone https://git.openwrt.org/openwrt/openwrt.git
 cd openwrt
-git checkout v21.02.1
+git checkout v21.02.2
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
