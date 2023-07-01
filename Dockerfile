04aff53f5d16a904ed107d738d096bb16fc51ab0
Grell Gragham <vrt.epm@protonmail.com>
2023-07-01 17:52:35 +0300

    refactor: change struct

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,19 @@
 FROM debian:bullseye
-RUN apt update -y &&\
-    apt install build-essential ccache ecj fastjar file g++ gawk \
-    gettext git java-propose-classpath libelf-dev libncurses5-dev \
-    libncursesw5-dev libssl-dev python python2.7-dev python3 unzip wget \
-    python3-distutils python3-setuptools python3-dev rsync subversion \
-    swig time xsltproc zlib1g-dev -y
+ARG WRT_DEPENDENCIES=""
+RUN apt update -y && apt install -y \
+	build-essential file g++ gawk gettext git \
+	libssl-dev python3-distutils rsync unzip vim wget \
+	$WRT_DEPS
+
 RUN useradd -m -s /bin/bash build
 USER build
-RUN mkdir /home/build/openwrt
-WORKDIR /home/build/openwrt
-ENTRYPOINT [ "/bin/bash", "/home/build/openwrt/build.sh" ]
+RUN mkdir /home/build/wrt
+WORKDIR /home/build/wrt
+
+ARG WRT_FIRMWARE_REPO=""
+ARG WRT_BRANCH=""
+RUN git clone -b "$WRT_BRANCH" --single-branch "$WRT_FIRMWARE" .
+RUN ./scripts/feeds update -a && ./scripts/feeds install -a
+
+COPY build.sh .
+ENTRYPOINT [ "/bin/bash", "/home/build/wrt/build.sh" ]
---
+++ build.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -e
+cd "$(dirname "$0")" || exit "$?"
+
+BUILD_PARAMETER="$1"
+CONFIG_DIR="./device_config"
+CONFIG_PATH="./$CONFIG_DIR/build.config"
+FIX_DIR="$CONFIG_DIR/fix"
+PATCH_DIR="$CONFIG_DIR/patch"
+BUILD_DIR="./bin"
+
+doNotRunAsRoot() {
+	if [[ $EUID == 0 ]]; then
+		echo "Don't run this script as root"
+		exit 1
+	fi
+}
+
+processFiles() {
+	local dir=$1
+	local pattern=$2
+	local action=$3
+
+	local regex="[0-9]*_$pattern"
+	local files=("$dir"/$regex)
+
+	if [[ -d "$dir" ]] && [[ -e "${files[0]}" ]]; then
+		for file in "${files[@]}"; do
+			echo "Processing $file..."
+			$action "$file"
+		done
+	else
+		echo "No valid files found in $dir."
+	fi
+}
+
+main() {
+	doNotRunAsRoot
+
+	case "$BUILD_PARAMETER" in
+	"preconfig")
+		processFiles "$PATCH_DIR" "*.patch" "patch -p1 <"
+		processFiles "$FIX_DIR" "*.sh" "bash"
+
+		cp "$CONFIG_PATH" .config
+		make defconfig
+		make download
+		make -j$(($(nproc) + 1))
+		;;
+	"manual")
+		make download
+		make menuconfig
+
+		if [[ -f .config ]]; then
+			cp .config "$BUILD_DIR"
+		fi
+
+		make -j$(($(nproc) + 1))
+		;;
+	"debug")
+		/bin/bash
+		;;
+	*)
+		echo "Invalid argument. Use 'preconfig', 'manual' or 'debug'."
+		exit 1
+		;;
+	esac
+}
+
+main
